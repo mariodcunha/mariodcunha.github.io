acc89c03ea87c24aa0289f400b68182ff0a83527
Mario Dcunha <mario.dcn@gmail.com>
2017-10-04 20:52:10 -0400

    test

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+// var i, j;
+// var hash = "#"
+// var array=[1,2];
+
+// for(i=1; i<8; i++)
+// {
+// 	for(j=0; j<i; j++)	
+// 	{
+// 		console.log(array);
+// 	}
+// 	console.log("\n");
+// }
+
+// // console.log("#");
+
+
+for(i=1; i<8; i++)
+{
+	for(j=0; j<i; j++)	
+	{
+		console.log(array);
+	}
+	console.log("\n");
+}
