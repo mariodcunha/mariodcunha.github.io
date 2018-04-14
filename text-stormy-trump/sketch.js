698af9f781198aa1dbcb9c96f2fbf68a6e592baf
Mario Dcunha <mario.dcn@gmail.com>
2018-04-14 01:47:13 -0400

    "

--- text-stormy-trump/sketch.js
+++ text-stormy-trump/sketch.js
@@ -16,6 +16,52 @@ Who do you think Trumped the other?
 
 */
 
+var mySounds=[], suddenSounds=[];
+
+function preload() 
+{
+  soundFormats('wav', 'mp3');
+
+  for (let index=1; index<11; index++) 
+  {
+    mySounds[index] = loadSound('sound/type'+ index +'.wav');
+    mySounds[index].amp(1);
+  }
+  stormSound = loadSound('sound/storm.mp3');
+  
+  for (let index=1; index<sentence3.length-1; index++) 
+  {
+    suddenSounds[index] = loadSound('sound/soundeffect-' + index + '.mp3');
+  }
+
+  bg = loadImage("images/trump.jpg");
+
+}
+
+function setup()
+{
+  stormSound.play();
+  stormSound.amp(0.5);
+
+  startTyping();
+}
+
+
+function draw()
+{
+  canvas = createCanvas(window.innerWidth/1.2, window.innerHeight);
+  // background(bg);
+  tint(255,255,255, tintValue[randomInt(tintValue.length)]);
+  image(bg, 0, 0, window.innerWidth/1.2, window.innerHeight);
+
+  // stormSound.play();
+  // stormSound.amp(0.3);
+
+  // startTyping();
+}
+
+
+
 
 var sentence1 = 
 {
@@ -23,12 +69,12 @@ var sentence1 =
   "Pronoun": ["He", "She"],
   "Verb": ["confusing", "hard", "challenging", "acting", "insulting", "scheming", "charging", "bloody", "standing smelly", "bruised and hurt"],
   "Position": ["#Place# table", "3rd street, away from the #Place#", "Crooked Chair, inside #Place#", "#Place# hallway"],
-  "Place": ["The Oval Office", "The White House", "Capitol Hill", "The Pentagon", "Congress Balcony", "the Golf Resort", "a KFC Chicken Place", "a Mexican Food Joint"]
+  "Place": ["Oval Office", "White House", "Capitol Hill", "Pentagon", "Congress Balcony", "Golf Resort", "KFC Chicken Place", "Mexican Food Joint"]
 }
 
 var sentence2 = 
 {
-  "S": ["He yelled #Adjective# rants at her, however she #Action# the #Things# at him.\n\n\n\n\n"],
+  "S": ["He yelled #Adjective# rants at her, however she #Action# the #Things# at him."],
   "Pronoun": ["he", "she"],
   "Adjective": ["nasty", "racist", "sexist", "xenophobic", "misogynistic", "bigoted", "clownish", "piggish", "climate change denying"],
   "Action": ["threw", "flung", "churned", "pushed", "lifted", "twisted"],
@@ -37,11 +83,13 @@ var sentence2 =
 }
 
 
-var sentence3 = 
-{
-  "S": ["Suddenly, there was a #Event#!"],
-  "Event": ["sound of an explosion", "loud cry", "scream", "squeak", "sound of several footsteps", "gunshot", "Fake News alert", "jolt. The ground shook", "hurricane landfall. The windows shattered"],
-}
+var sentence3 = ["Suddenly, there was a ", "loud cry", "hurricane landfall. The windows shattered", "scream", "sound of an explosion", "gunshot", "Fake News alert", "jolt. The ground shook", "!"];
+
+// var sentence3 = 
+// {
+//   "S": ["Suddenly, there was a #Event#!"],
+//   "Event": ["sound of an explosion", "scream", "squeak", "sound of several footsteps", "gunshot", "Fake News alert", "jolt. The ground shook", ],
+// }
 
 
 var sentence4 = 
@@ -54,10 +102,13 @@ var sentence4 =
 var Sentence = [];
 var Story = [];
 var i=0, j=0, k=0, m=0, n=0, x=0, y=0;
+var speedOfTyping = 100, suddenText=0;
+var tintValue = [0,0,0,0,0,0,0,0,10,100];
 
 Sentence[0] = tracery.createGrammar(sentence1);
 Sentence[1] = tracery.createGrammar(sentence2);
-Sentence[2] = tracery.createGrammar(sentence3);
+// Sentence[2] = tracery.createGrammar(sentence3);
+// Sentence[2] = sentence3;
 Sentence[3] = tracery.createGrammar(sentence4);
 
 
@@ -80,8 +131,13 @@ function typeWriter2()
 }
 
 function typeWriter3()
-{   
-    Story[2] = Sentence[2].flatten("#S#"); type3();
+{
+    suddenText = randomInt(1,(sentence3.length)-1);
+    Story[2] = sentence3[0] + sentence3[suddenText] + sentence3[(sentence3.length)-1];
+    
+    console.log(suddenText);
+    
+    type3();
 }
 
 function typeWriter4()
@@ -97,6 +153,14 @@ function typeWriter5()
     type5();   
 }
 
+function typewriterSound()
+{
+  var randomTypeWriterSound = randomInt(1,11);
+  mySounds[randomTypeWriterSound].setVolume(1.0);
+  mySounds[randomTypeWriterSound].play();
+  mySounds[randomTypeWriterSound].setVolume(1.0);
+
+}
 
 function type0() 
 {
@@ -106,14 +170,18 @@ function type0()
     if(x<18 || x>24)
     {
       $('#headline0').append(Story_0.charAt(x++));
+      typewriterSound();
     }  
     else if(x>=18 && x<=24)
     {
       $('<span id="stormy"></span>').appendTo('#headline0');
       $('#headline0').append(Story_0.charAt(x++));
+      mySounds[randomInt(1,11)].play();
     }
-    
-    setTimeout(type0, 50);
+    if(x>=18 && x<=24)
+      setTimeout(type0, speedOfTyping*2);
+    else
+      setTimeout(type0, speedOfTyping);
   }
   else if(x>=Story_0.length)
       typeWriter1();
@@ -127,7 +195,8 @@ function type1()
   if(j<Story[0].length) 
   {
     $('#headline1').append(Story[0].charAt(j++));
-    setTimeout(type1, 50);
+    mySounds[randomInt(1,11)].play();
+    setTimeout(type1, speedOfTyping);
   }
   else if(j>=Story[0].length)
       typeWriter2();
@@ -139,7 +208,9 @@ function type2()
   if(k<Story[1].length) 
   {
     $('#headline1').append(Story[1].charAt(k++));
-    setTimeout(type2, 50);
+    mySounds[randomInt(1,11)].play();
+    // $('#voice').attr('src', 'sound/type'+randomMath(1,5)+'.wav');
+    setTimeout(type2, speedOfTyping);
   }
   else if(k>=Story[1].length)
       typeWriter3();
@@ -151,10 +222,19 @@ function type3()
   if(m<Story[2].length) 
   {
     $('#headline2').append(Story[2].charAt(m++));
-    setTimeout(type3, 50);
+    mySounds[randomInt(1,11)].play();
+
+    if(m>=0 && m<=9)
+      setTimeout(type3, speedOfTyping*2);
+    else
+      setTimeout(type3, speedOfTyping);
   }
   else if(m>=Story[2].length)
+  {
+      suddenSounds[suddenText].play();
+      suddenSounds[suddenText].amp(0.7);
       typeWriter4();
+  }
 }
 
 
@@ -163,7 +243,8 @@ function type4()
   if(n<Story[3].length) 
     {
       $('#headline3').append(Story[3].charAt(n++));
-      setTimeout(type4, 50);
+      mySounds[randomInt(1,11)].play();
+      setTimeout(type4, speedOfTyping);
     }
   else if(n>=Story[3].length)
       typeWriter5();
@@ -175,24 +256,28 @@ function type5()
   if(y<Story_last.length) 
   {
     $('#headline4').append(Story_last.charAt(y++));
-    // if(x<18 || x>24)
-    // {
-    //   $('#headline0').append(Story_0.charAt(x++));
-    // }  
-    // else if(x>=18 && x<=24)
-    // {
-    //   $('<span id="stormy"></span>').appendTo('#headline0');
-    //   $('#headline0').append(Story_0.charAt(x++));
-    // }
-    
-    setTimeout(type5, 50);
+    mySounds[randomInt(1,11)].play();    
+
+    if(y>=29 && y<=34)
+      setTimeout(type5, speedOfTyping*2);
+    else
+      setTimeout(type5, speedOfTyping);
   }
-  // else if(x>=Story_0.length)
-  //     typeWriter1();
 }
 
 
 
+function  randomMath(min, max)
+{
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+function  randomInt(l,h)
+{
+  return Math.floor(random(l,h));
+}
 
 
 
