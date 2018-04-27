9dd244d56f3416039d48a09086f4b406e2db5c2c
Mario Dcunha <mario.dcn@gmail.com>
2018-04-26 23:13:58 -0400

    "

--- music-djen/sketch.js
+++ music-djen/sketch.js
@@ -1,38 +1,51 @@
 
 
+
+
 let synth;
-var waves = ['triangle','sine','sawtooth','square'];
+var waves = ['triangle','sine','square'];
 
 function setup() 
 {
-    createCanvas(400, 200);
+    createCanvas(window.innerWidth, window.innerHeight);
 
-    const startButton = createButton('start');
-    startButton.mousePressed(start);
+    // const startButton = createButton('start');
+    // startButton.mousePressed(start);
+
+    imageMode(CENTER);
+    diskImage = loadImage("disk.png"); 
 
 }
 
 function draw() 
 {
     background(50);
+    image(diskImage, window.innerWidth/2-window.innerWidth/6, window.innerHeight/2, window.innerWidth/3,window.innerWidth/3);
+    image(diskImage, window.innerWidth/2+window.innerWidth/6, window.innerHeight/2, window.innerWidth/3,window.innerWidth/3);
 }
 
 
 function start() 
 {
+
     var melody=[];    
     var rhythm=randomInt(4,16);
 
-    synth = new SimpleSynth(waves[randomInt(0,waves.length)]);
+    synth = new SimpleSynth(waves[randomInt(0,waves.length-1)]);
 
     
-    for(let i=45, j=0; j<40; i=i+randomInt(-10,10), j++)
+    for(let i=45, j=0; j<40; i=i+randomInt(-4,4), j++)
     {
-        melody[j] = [i,rhythm/rhythm];
+        melody[j] = [i,rhythm/16];
     }
    
     synth.playNotes(melody);
 
+}
+
+function mousePressed()
+{
+    start();
 }
 
 
@@ -40,3 +53,8 @@ function randomInt(l,h)
 {
     return Math.floor(random(l,h));
 }
+
+
+
+
+
