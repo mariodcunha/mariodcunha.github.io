70bbb2720b1f2c4810b66e44e48d2da339776835
Mario Dcunha <mario.dcn@gmail.com>
2018-08-25 01:52:03 -0400

    "

---
+++ resume/js/my-javascript.js
@@ -0,0 +1,19 @@
+
+
+// console.log("kakka");
+
+// $('#contactMeHeading').mouseover(function() 
+// {
+// 	alert("test");
+//   // $( "#contactMeDrop" ).css("display","initial");
+// });
+
+
+$(".resumeHeading").mouseover
+(
+	function()
+	{
+		alert("test");
+	}
+);
+
