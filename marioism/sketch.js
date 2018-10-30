f454eba2bfd4af796469e450bdc040d8d713a0af
Mario Dcunha <mario.dcn@gmail.com>
2018-10-30 02:16:30 -0400

    "

--- marioism/sketch.js
+++ marioism/sketch.js
@@ -111,6 +111,7 @@ inspiration[11] = new Quote("Where there is righteousness in the heart, there is
 inspiration[12] = new Quote("The more you are clear of who you are, the more awesome work you will do.", "Lucille Tenazas");
 inspiration[13] = new Quote("If you fall off a horse, then get on a donkey, until you find your next horse.", "Morry Galonoy");
 inspiration[14] = new Quote("We are the process. We are not just part of it.", "Archit Kaushik");
+inspiration[15] = new Quote("Speaking is silver, but silence is Golden. ", "The Egyptians, Thomas Carlyle, my late Grandpa.");
 
 
 //DESIGN
