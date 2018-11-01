18f891c04ec87dd7a76114698891a90f2dad0cce
Mario Dcunha <mario.dcn@gmail.com>
2018-11-01 00:49:00 -0400

    "

--- marioism/sketch.js
+++ marioism/sketch.js
@@ -110,7 +110,7 @@ inspiration[10] = new Quote("Order is Heaven's first law", "Alexander Pope");
 inspiration[11] = new Quote("Where there is righteousness in the heart, there is beauty in the character. When there is beauty in the character, there is harmony in the home. When there is harmony in the home, there is order in the nation. When there is order in the nation, there is peace in the world.", "President Abdul Kalam");
 inspiration[12] = new Quote("The more you are clear of who you are, the more awesome work you will do.", "Lucille Tenazas");
 inspiration[13] = new Quote("If you fall off a horse, then get on a donkey, until you find your next horse.", "Morry Galonoy");
-inspiration[14] = new Quote("We are the process. We are not just part of it.", "Archit Kaushik");
+inspiration[14] = new Quote("We are not just part of the process. We are the process.", "Archit Kaushik");
 inspiration[15] = new Quote("Speaking is silver, but silence is Golden. ", "The Egyptians, Thomas Carlyle, my late Grandpa.");
 
 
